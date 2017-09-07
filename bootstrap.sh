1e98e4ccf1ebaa6a8d25b86322072423cb291ed3
Kyle Robertson <kyle.wesley@me.com>
2017-09-07 10:34:08 -0400

    Added CLI
    Added CLI to the deploy script. Might need a better name than deploy but
    it's fine for now. Just has capability to deploy and delete right now.
    Might want to add subcommand for suspending the instance without
    deleting everything.
    
    Also added some helpful print commands to the bootstrap script to see
    what stage the bootstrap is in on stdout

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 set -e
 
+echo "############## Upgrading and Installing standard packages ###############"
 apt update
 apt upgrade -y
-apt install -y git vim python3-pip build-essential
+apt install -y git vim python3-pip build-essential libopenblas-dev \
+               liblapack-dev python-dev
 # Install Cuda 8 which we'll need for tensorflow
+echo "############## Retrieving and Installing CUDA ###############"
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
 dpkg -i cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
 apt-get update
 apt-get install -y cuda
+echo "############## CUDA Installion Complete! ###############"
 # Install cuDNN, which we'll also need for tensorflow
-tar -xzvf cudnn-8.0-linux-x64-v6.0.tgz
+echo "############## Unpacking and Installing cuDNN ###############"
+tar -xzf cudnn-8.0-linux-x64-v6.0.tgz
 cp cuda/include/cudnn.h /usr/local/cuda-8.0/include
 cp cuda/lib64/libcudnn* /usr/local/cuda-8.0/lib64
 chmod a+r /usr/local/cuda-8.0/include/cudnn.h
+echo "############## cuDNN Installation Complete! ###############"
 # Add the path's for cuda library files to .bashrc
+export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH
 echo 'export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH' >> \
     /home/ubuntu/.bashrc 
+# Need to add cuda binaries to the path if we ever want to install theano or
+# pycuda or other such things
+echo 'export PATH=/usr/local/cuda-8.0/bin:$PATH' >> /home/ubuntu/.bashrc 
+# For root as well
+export CUDA_ROOT=/usr/local/cuda-8.0
 # Install python modules
-pip3 install numpy scipy matplotlib pandas jupyter tensorflow-gpu
+echo "############## Installing Python Modules ###############"
+pip3 install numpy scipy matplotlib pandas jupyter tensorflow-gpu scikit-learn keras
+echo "############## Python Modules Installed! ###############"
+# Need to reboot to load nvidia drivers
+echo "############## Rebooting now! ###############"
+reboot now
--- deploy.py
+++ deploy.py
@@ -1,14 +1,16 @@
+import argparse as ap
+from datetime import datetime
+from botocore.exceptions import ClientError
+from itertools import zip_longest
 import os
 import boto3
 import paramiko
-from botocore.exceptions import ClientError
-from datetime import datetime
 
-def create_keypair(name="YazabiServerKeypair"):
+def create_keypair(name):
     """
-    Creates a keypair in AWS. If name is not provided, it defaults to
-    YazabiServerKeypair for the name
+    Creates a keypair with the given name in AWS. 
     """
+
     print('Creating AWS keypair ...')
     ec2 = boto3.client('ec2')
     try:
@@ -32,15 +34,15 @@ def create_keypair(name="YazabiServerKeypair"):
     os.chmod(keyfilename, 0o600)
     return name
 
-def deploy_template(keyname):
+def deploy_template(keyname, stackname):
     """
     Deploys the template file using aws cloudformation
     """
+
     cf = boto3.client('cloudformation')
     tpath = 'templates/server_elasticIP_ebsrootvol.yaml'
     with open(tpath) as template:
         body = template.read()
-    stackname = "YazabiServerStack"
     try:
         response = cf.create_stack(
             StackName=stackname,
@@ -84,12 +86,19 @@ def deploy_template(keyname):
 
 def printTotals(transferred, toBeTransferred):
     print("Transferred: {:.2f} MB\tOut of: {:.2f} MB".format(transferred/(1000*1000),
-                                                 toBeTransferred/(1000*1000)),
-          end='\r')
+                                                             toBeTransferred/(1000*1000)),
+                                                             end='\r')
 
 def upload_cudnn(ip, keyfile):
+    """
+    Opens a paramiko ssh connection and uses sftp to upload the gzipped tar
+    file of the cuDNN libraries we need. Unfortunately this step is a bit
+    manual because the cuDNN libraries are hidden behind a login wall and I
+    couldn't find a way to pull them down automatically
+    """
+
     print('We need to upload the cuDNN libraries you downloaded!')
-    ssh = paramiko.SSHClient() 
+    ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(ip, username='ubuntu', key_filename=keyfile)
     sftp = ssh.open_sftp()
@@ -97,7 +106,7 @@ def upload_cudnn(ip, keyfile):
                        '[./cudnn-8.0-linux-x64-v6.0.tgz]: ')
     if not cudnn_path:
         print('no entry')
-        cudnn_path = os.path.join(os.getcwd(),'cudnn-8.0-linux-x64-v6.0.tgz')
+        cudnn_path = os.path.join(os.getcwd(), 'cudnn-8.0-linux-x64-v6.0.tgz')
     remotepath = '/home/ubuntu/cudnn-8.0-linux-x64-v6.0.tgz'
     print('Beginning upload ...')
     sftp.put(cudnn_path, remotepath, callback=printTotals)
@@ -106,6 +115,11 @@ def upload_cudnn(ip, keyfile):
     return ssh
 
 def run_bootstrap(client):
+    """
+    Uploads the bootstrap script to the server and executes it. Expects a
+    connected paramiko SSH client as an argument.
+    """
+
     sftp = client.open_sftp()
     print('Uploading bootstrap script ...')
     sftp.put('./bootstrap.sh', '/home/ubuntu/bootstrap.sh', callback=printTotals)
@@ -114,20 +128,29 @@ def run_bootstrap(client):
     print("Executing bootstrap script ...")
     stdin, stdout, stderr = client.exec_command("sudo bash bootstrap.sh")
     stdin.close()
-    for line in iter(stdout.readline, ""):
-        print(line, end="")
+    for oline, eline in zip_longest(iter(stdout.readline, ""),
+                                    iter(stderr.readline, "")):
+        if oline is not None:
+            print(oline, end="")
+        if eline is not None:
+            print(eline, end="")
+    # for line in iter(stdout.readline, ""):
+    # for line in iter(stderr.readline, ""):
+    #     print(line, end="")
+    print('\nBootstrapping complete!\n')
+    print('Instance is currently rebooting, access may be down for a few'
+          ' minutes')
 
-def main():
+def deploy(args):
+    """
+    Wrapper function to tie together all the steps for deployment
+    """
     # We need to create the SSH keypair first because AWS CloudFormation doesn't
     # support creating keypairs. We create it within AWS so we need not assume
     # anything about existing key generation tools
-    keyname = create_keypair()
+    keyname = create_keypair(args.keyname)
     # Now we need to deploy the template.
-    server_ip = deploy_template(keyname)
-    # server_ip = '18.220.30.107'
-    # keyname = "YazabiServerKeypair"
-    print(server_ip)
-    print(keyname)
+    server_ip = deploy_template(keyname, args.stackname)
     # Copy up the cuDNN libs
     ssh_client = upload_cudnn(server_ip, '{}.pem'.format(keyname))
     # Finally, run the bootstrap script
@@ -138,6 +161,69 @@ def main():
         print('    ssh -i {}.pem ubuntu@{}'.format(keyname, server_ip))
     else:
         raise ValueError('Did not receive an IP address')
+
+def delete(args):
+    """
+    Wrapper function to delete a stack. The name of the stack to delete is
+    optional. Otherwise, this function will collect a list of deployed stacks
+    and present them to the user, allowing them to choose which stack to delete
+    or to cancel the operation
+    """
+    cf = boto3.client('cloudformation')
+    ec2 = boto3.client('ec2')
+    if args.stackname is not None:
+        stackname = args.stackname
+    else:
+        resp = cf.describe_stacks()
+        print(resp['Stacks'])
+        names = [stack['StackName'] for stack in resp['Stacks']]
+        if not names:
+            print('There are no deployed stacks!')
+            return
+        msg_list = ""
+        for i, name in enumerate(names):
+            msg_list += "{}. {}\n".format(i+1, name)
+        msg = "Here are the stacks you have deployed:\n"+msg_list
+        msg += "Please choose the number of the stack you wish to delete: "
+        stack_num = None
+        while not isinstance(stack_num, int) or stack_num == 0:
+            try:
+                stack_num = int(input(msg))
+            except ValueError:
+                continue
+        stackname = names[stack_num-1]
+    info = cf.describe_stacks(StackName=args.stackname)
+    keyname = ""
+    for par in info['Parameters']:
+        if par['ParameterKey'] == 'KeyName':
+            keyname = par['ParameterValue']
+    if keyname:
+        print('Deleting keypair {}'.format(keyname))
+        ec2.delete_key_pair(KeyName=keyname)
+    print('Deleting stack {}'.format(stackname))
+    cf.delete_stack(StackName=names[stackname])
+    delete_complete_waiter = cf.get_waiter('stack_delete_complete')
+    delete_complete_waiter.wait(StackName=stackname)
+    print('Deletion complete!')
+
+def main():
+    parser = ap.ArgumentParser(description="""A tool for deploying a single
+    server, backed by a persistent EBS volume and with a public Elastic IP
+    address to AWS automatically via CloudFormation. This tool will also run a
+    bootstrap script to get you up and running with Tensorflow on Python3""")
+    subparsers = parser.add_subparsers(dest="subparser_name", help="[sub-command] help]")
+    parser_deploy = subparsers.add_parser("deploy", help="Deploy the stack")
+    parser_deploy.add_argument('--stackname', default="YazabiServerStack",
+                               help="""The name of the stack to deploy""")
+    parser_deploy.add_argument('--keyname', default="YazabiServerKeypair",
+                               help="""The name of the keypair to deploy""")
+    parser_deploy.set_defaults(func=deploy)
+    parser_delete = subparsers.add_parser("delete", help="Delete a stack")
+    parser_delete.add_argument('--stackname', help="""The name of the stack to
+    delete""")
+    parser_delete.set_defaults(func=delete)
+    args = parser.parse_args()
+    args.func(args)
 
 
 if __name__ == "__main__":
